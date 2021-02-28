899f6a38357e8f4dbe9bde258018402b76360cfa
jordanrob709 <jordanrob709@gmail.com>
2021-02-28 20:25:31 +0300

    Exercise 5.18: bloglist end to end testing, step2

--- cypress/integration/blog_app.spec.js
+++ cypress/integration/blog_app.spec.js
@@ -1,6 +1,12 @@
 describe('Blog app', function() {
     beforeEach(function() {
       cy.request('POST', 'http://localhost:3001/api/testing/reset')
+      const user = {
+          name: 'Jona',
+          username:'jod3s',
+          password:'jona2021'
+      }
+      cy.request('POST', 'http://localhost:3001/api/users', user)
       cy.visit('http://localhost:3000')
     })
   
@@ -8,5 +14,25 @@ describe('Blog app', function() {
       cy.contains('login')
       cy.contains('username')
       cy.contains('password')
+    })
+
+    describe('login', function() {
+        it('succeeds with correct credentials', function(){
+            cy.get('#username').type('jod3s')
+            cy.get('#password').type('jona2021')
+            cy.get('#login-button').click()
+            cy.contains('Jona is logged in')
+        })
+        
+        it('fails with wrong credentials', function(){
+            cy.get('#username').type('jod3s')
+            cy.get('#password').type('jona2021234')
+            cy.get('#login-button').click()
+            cy.contains('wrong credentials entered')
+
+            cy.get('.error')
+              .should('have.css', 'color', 'rgb(255, 0, 0)')
+        })
+
     })
   })
--- src/App.js
+++ src/App.js
@@ -61,6 +61,7 @@ const App = () => {
         <div>
           username
           <input
+          id="username"
           type = "text"
           value = {username}
           onChange = {({ target }) => setUsername(target.value) }
@@ -71,13 +72,14 @@ const App = () => {
         <div>
           password
           <input
+          id="password"
           type = "password"
           value = {password}
           name = "Password"
           onChange = { ({target}) => setPassword(target.value) }
           />
         </div>  
-        <button type="submit">login</button>
+        <button id="login-button" type="submit">login</button>
       </form>
     
   )
